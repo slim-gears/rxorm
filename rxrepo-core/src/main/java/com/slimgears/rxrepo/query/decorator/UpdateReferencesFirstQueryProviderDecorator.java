c3e9a78715ea717f3428842dfa62628b728be80c
denis-itskovich <denis.itskovich@dell.com>
2019-10-28 10:05:16 +0200

    [rxrepo-core] Insert references instead of update

--- rxrepo-core/src/main/java/com/slimgears/rxrepo/query/decorator/UpdateReferencesFirstQueryProviderDecorator.java
+++ rxrepo-core/src/main/java/com/slimgears/rxrepo/query/decorator/UpdateReferencesFirstQueryProviderDecorator.java
@@ -1,5 +1,7 @@
 package com.slimgears.rxrepo.query.decorator;
 
+import com.slimgears.rxrepo.expressions.PropertyExpression;
+import com.slimgears.rxrepo.query.provider.QueryInfo;
 import com.slimgears.rxrepo.query.provider.QueryProvider;
 import com.slimgears.rxrepo.util.PropertyMetas;
 import com.slimgears.util.autovalue.annotations.MetaClassWithKey;
@@ -28,37 +30,42 @@ public class UpdateReferencesFirstQueryProviderDecorator extends AbstractQueryPr
     @Override
     public <K, S> Completable insert(MetaClassWithKey<K, S> metaClass, Iterable<S> entities) {
         return Observable.fromIterable(entities)
-                .concatMapEager(e -> insertOrUpdateReferences(metaClass, e).andThen(Observable.just(e)))
+                .concatMapEager(e -> insertReferences(metaClass, e).andThen(Observable.just(e)))
                 .ignoreElements()
                 .andThen(super.insert(metaClass, entities));
     }
 
     @Override
     public <K, S> Single<S> insertOrUpdate(MetaClassWithKey<K, S> metaClass, S entity) {
-        return insertOrUpdateReferences(metaClass, entity).andThen(super.insertOrUpdate(metaClass, entity));
+        return insertReferences(metaClass, entity).andThen(super.insertOrUpdate(metaClass, entity));
     }
 
     @Override
     public <K, S> Maybe<S> insertOrUpdate(MetaClassWithKey<K, S> metaClass, K key, Function<Maybe<S>, Maybe<S>> entityUpdater) {
         return super.insertOrUpdate(metaClass, key, maybeEntity -> entityUpdater
                 .apply(maybeEntity)
-                .flatMap(updatedEntity -> insertOrUpdateReferences(metaClass, updatedEntity)
+                .flatMap(updatedEntity -> insertReferences(metaClass, updatedEntity)
                         .andThen(Maybe.just(updatedEntity))));
     }
 
-    private <K, S> Completable insertOrUpdateEntity(MetaClassWithKey<K, S> metaClass, S entity) {
-        return insertOrUpdate(metaClass, entity)
-                .doOnSubscribe(d -> log.debug("Inserting entity: {}", entity))
-                .ignoreElement();
+    private <K, S> Completable insertEntity(MetaClassWithKey<K, S> metaClass, S entity) {
+        return query(QueryInfo
+            .<K, S, S>builder()
+            .metaClass(metaClass)
+            .limit(1L)
+            .predicate(PropertyExpression.ofObject(metaClass.keyProperty()).eq(metaClass.keyOf(entity)))
+            .build())
+            .firstElement()
+            .switchIfEmpty(Single.defer(() -> insertOrUpdate(metaClass, entity)))
+            .ignoreElement();
     }
 
-    private <K, S> Completable insertOrUpdateReferences(MetaClassWithKey<K, S> metaClass, S entity) {
+    private <K, S> Completable insertReferences(MetaClassWithKey<K, S> metaClass, S entity) {
         return Observable.fromIterable(metaClass.properties())
                 .filter(PropertyMetas::isReference)
                 .flatMapCompletable(p -> Optional
                                 .ofNullable(p.getValue(entity))
-                                .map(val -> insertOrUpdateReferences(MetaClasses.forTokenWithKeyUnchecked(p.type()), val)
-                                        .andThen(insertOrUpdateEntity(MetaClasses.forTokenWithKeyUnchecked(p.type()), val)))
+                                .map(val -> insertEntity(MetaClasses.forTokenWithKeyUnchecked(p.type()), val))
                                 .orElseGet(Completable::complete));
     }
 }
